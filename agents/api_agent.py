2b58710617587f0fe031ffb2908931e493f00cda
gopal-crossml <gopal.chopra@crossml.com>
2026-01-05 13:17:19 +0530

    updated code

--- agents/api_agent.py
+++ agents/api_agent.py
@@ -4,9 +4,10 @@ from langchain.agents import create_agent
 from cred import gemini_api_key
 from tools.weather import get_weather
 from .multi_tools import llm
+from prompts import system_prompt
 
 api_agent = create_agent(
     model=llm,
     tools=[get_weather],
-    system_prompt="you are a supportive assistant"
+    system_prompt=system_prompt
 )
